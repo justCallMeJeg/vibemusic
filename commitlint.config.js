7fff7a2273a7871f1901e123756e0596eb1684be
gjpgabayeron <gjpgabayeron@gmail.com>
2026-01-12 21:12:34 +0800

    feat: commitlint for commit message linting

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation
+        "style", // Formatting, no code change
+        "refactor", // Code restructuring
+        "perf", // Performance improvement
+        "test", // Adding tests
+        "chore", // Maintenance
+        "ci", // CI/CD changes
+        "build", // Build system changes
+        "revert", // Revert previous commit
+      ],
+    ],
+    "subject-case": [2, "always", "lower-case"],
+    "subject-empty": [2, "never"],
+    "type-empty": [2, "never"],
+  },
+};
